8a9298e2ed2b1a729794f3dc5b666c579a08f637
brainburster <brainburster@outlook.com>
2021-06-17 01:54:45 +0800

    bugfixes

--- SoftRasterLearning/SoftRasterRenderer.h
+++ SoftRasterLearning/SoftRasterRenderer.h
@@ -2,23 +2,50 @@
 #include "DC_WND.hpp"
 #include "triangles_view.hpp"
 #include "buffer_view.hpp"
+#include <vector>
 
-class SoftRasterRenderer
+class SoftRasterRenderer_Context
 {
 public:
-	using FragmentBuffer = Buffer2DView<Color32>;
-	using DepthBuffer = Buffer2DView<float>;
 
-
-	void DrawToBuffer() 
+	void CopyToScreen(Buffer2DView<Color32>& screen_buffer_view)
 	{
-		
+		if (screen_buffer_view.w == m_fragment_buffer_view.w && screen_buffer_view.h == m_fragment_buffer_view.h)
+		{
+			memcpy(screen_buffer_view.buffer, m_fragment_buffer_view.buffer, screen_buffer_view.w * screen_buffer_view.h);
+		}
+		else 
+		{
+			for (uint32 y=0;y<screen_buffer_view.h;++y)
+			{
+				for (uint32 x = 0; x < screen_buffer_view.w; ++x)
+				{
+					screen_buffer_view.Set(x, y, m_fragment_buffer_view.Get(x, y));
+				}
+			}
+		}
 	}
-	void PrintToScreen() 
-	{
 
+	void Viewport(uint32 w, uint32 h) 
+	{
+		m_depth_buffer.reserve(w * h);
+		m_fragment_buffer.reserve(w * h);
+		m_fragment_buffer_view = { &m_fragment_buffer[0],w , h };
+		m_depth_buffer_view = { &m_depth_buffer[0],w , h };
+	}
+
+	const Buffer2DView<Color32>& GetFragBufferView() 
+	{
+		return m_fragment_buffer_view;
+	}
+
+	const Buffer2DView<float>& GetDepthBufferView()
+	{
+		return m_depth_buffer_view;
 	}
 private:
-	FragmentBuffer m_fragment_buffer;
-	DepthBuffer m_depth_buffer;
+	Buffer2DView<Color32> m_fragment_buffer_view;
+	Buffer2DView<float> m_depth_buffer_view;
+	std::vector<Color32> m_fragment_buffer;
+	std::vector<float> m_depth_buffer;
 };
--- SoftRasterLearning/buffer_view.hpp
+++ SoftRasterLearning/buffer_view.hpp
@@ -14,13 +14,13 @@ struct Buffer2DView
 	void Set(uint32 x, uint32 y, T v)
 	{
 		size_t index = (size_t)y * w + x;
-		if (index<0 || index>w * h - 1) return;
+		if (index<0 || index>w * h - 1) return ;
 		buffer[index] = v;
 	}
 	T Get(uint32 x, uint32 y)
 	{
 		size_t index = (size_t)y * w + x;
-		if (index<0 || index>w * h - 1) return;
+		if (index<0 || index>w * h - 1) return {0};
 		return buffer[index];
 	}
 
