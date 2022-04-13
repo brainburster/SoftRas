70f47c2d1f6cd6911ce3e734418c1d933106c7dd
brainburster <brainburster@outlook.com>
2022-04-13 17:33:02 +0800

    对软光栅核心代码添加了详细注释

--- SoftRasterLearning/core/software_renderer.hpp
+++ SoftRasterLearning/core/software_renderer.hpp
@@ -4,20 +4,17 @@
 
 namespace core
 {
-	//默认着色器
-	class Shader_Default
+	//默认着色器，什么都没干
+	class ShaderDefault
 	{
 	public:
-		using VS_IN = Vertex_Default;
-		using VS_OUT = Vertex_Default;
-		using FS_OUT = Color;
-
-		VS_OUT VS(const VS_IN& v)
+		// Vertex_Default 继承自 vs_out_base 模板类，这个模板类重载了加法与乘法
+		Vertex_Default VS(const Vertex_Default& v)
 		{
 			return v;
 		}
 
-		FS_OUT FS(const VS_OUT& v)
+		Color FS(const Vertex_Default& v)
 		{
 			return v.color;
 		}
@@ -25,25 +22,25 @@ namespace core
 
 	enum ERenderFlag
 	{
-		RF_CULL_FRONT = 1,
-		RF_CULL_BACK = 2,
-		RF_CULL_CVV_SIMPLE = 4,
-		RF_CULL_CVV_CLIP = 8,
+		RF_CULL_FRONT = 1, //剔除正面 顺时针(我定义的正面是顺时针)
+		RF_CULL_BACK = 2,  //剔除反面 逆时针
+		RF_CULL_CVV_SIMPLE = 4, //简单的CVV剔除，三角形的顶点只要有一个在CVV之外就全部丢弃掉
+		RF_CULL_CVV_CLIP = 8,   //三角形3个顶点都在CVV外面的情况，全部丢弃
 		//RF_ENABLE_MULTI_THREAD = 16,
-		RF_ENABLE_SIMPLE_AA = 32,
-		RF_ENABLE_BLEND = 64,
-		RF_ENABLE_DEPTH_TEST = 128,
+		RF_ENABLE_SIMPLE_AA = 32, //简单的抗锯齿，不带采样点深度缓存的，建议不要用
+		RF_ENABLE_BLEND = 64,     //打开透明度混合，
+		RF_ENABLE_DEPTH_TEST = 128, //打开深度测试
 		//...
 		RF_DEFAULT = RF_CULL_BACK | RF_CULL_CVV_CLIP /*| RF_ENABLE_MULTI_THREAD */ | RF_ENABLE_BLEND | RF_ENABLE_DEPTH_TEST,
 		RF_DEFAULT_AA = RF_DEFAULT | RF_ENABLE_SIMPLE_AA
 	};
 
 	//渲染器类
-	template<typename Shader = Shader_Default, size_t render_flag = RF_DEFAULT>
+	template<typename Shader = ShaderDefault, size_t render_flag = RF_DEFAULT>
 	class Renderer
 	{
 	private:
-		//用来萃取顶点/像素着色器的输入、输出类型
+		//用来萃取顶点/像素着色器的输入、输出类型，利用了函数模板类型推导，得到了返回值和输入类型，这里是只有一个输入的情况，想要获得多个输入类型的信息可以使用可变参数模板，比如typename In..., 获得其中某个类型信息的可以使用std::tuple相关的方法
 		template <typename T, typename R, typename In>
 		static In get_in_type(R(T::* f)(In) const volatile) {}
 		template <typename T, typename R, typename In>
@@ -62,13 +59,14 @@ namespace core
 		static R get_out_type(R(T::* f)(In)) {}
 
 	public:
-		using vs_in_t = std::decay_t<decltype(get_in_type<Shader>(std::declval<decltype(&Shader::VS)>()))>;
-		using vs_out_t = std::decay_t<decltype(get_out_type<Shader>(std::declval<decltype(&Shader::VS)>()))>;
-		using fs_in_t = std::decay_t<decltype(get_in_type<Shader>(std::declval<decltype(&Shader::FS)>()))>;
-		using fs_out_t = std::decay_t<decltype(get_out_type<Shader>(std::declval<decltype(&Shader::FS)>()))>;
-		//断言shader的合法性
+		using vs_in_t = std::decay_t<decltype(get_in_type<>(std::declval<decltype(&Shader::VS)>()))>; //declval是一个没有被实现的函数，它的返回值是一个T类型的引用，它仅仅应该出现在decltype中参与编译器类型推导
+		using vs_out_t = std::decay_t<decltype(get_out_type<>(std::declval<decltype(&Shader::VS)>()))>;
+		using fs_in_t = std::decay_t<decltype(get_in_type<>(std::declval<decltype(&Shader::FS)>()))>;
+		using fs_out_t = std::decay_t<decltype(get_out_type<>(std::declval<decltype(&Shader::FS)>()))>;
+
+		//断言shader的合法性,顶点着色器的输入类型必须与像素着色器的输出类型相同(可以被const修饰，可以为引用)，而且顶点着色器的输出类型必须CRTP得继承自vs_out_base，这个模板类重载了+和*，并使用sse做了加速（不过编译器好像本来就能加速这个）
 		static_assert(std::is_base_of_v<vs_out_base<vs_out_t>, vs_out_t>, "the output type of vs_shader must be inherited from vs_out_base");
-		static_assert(std::is_same_v<vs_out_t, fs_in_t>, "the output ype of vs_shader must be the same as the input type of the fs_shader");
+		static_assert(std::is_same_v<vs_out_t, fs_in_t>, "the output type of vs_shader must be the same as the input type of the fs_shader");
 
 		Renderer(Context<fs_out_t>& ctx, const Shader& m) :
 			context{ ctx },
@@ -76,6 +74,7 @@ namespace core
 		{
 		}
 
+		// 通过顶点数组和索引数组绘制三角形，n为索引数组长度
 		void DrawIndex(vs_in_t* data, size_t* index, size_t n)
 		{
 			for (size_t i = 0; i < n; i += 3)
@@ -84,6 +83,7 @@ namespace core
 			}
 		}
 
+		// 绘制n/3个三角形
 		void DrawTriangles(vs_in_t* data, size_t n)
 		{
 			for (size_t i = 0; i < n; i += 3)
@@ -92,6 +92,7 @@ namespace core
 			}
 		}
 
+		// 绘制一个三角形
 		void DrawTriangle(vs_in_t* p0, vs_in_t* p1, vs_in_t* p2)
 		{
 			if constexpr (bool(render_flag & RF_CULL_CVV_SIMPLE))
@@ -132,6 +133,15 @@ namespace core
 					RasterizeTriangle(polygon, polygon + i - 1, polygon + i);
 				}
 			}
+			else {
+				//本地空间 => 裁剪空间 clip space
+				vs_out_t triangle[3] = {
+					{ shader.VS(*p0) },
+					{ shader.VS(*p1) },
+					{ shader.VS(*p2) }
+				};
+				RasterizeTriangle(triangle, triangle + 1, triangle + 2);
+			}
 		}
 	protected:
 
@@ -145,7 +155,7 @@ namespace core
 			};
 
 			//转化为屏幕坐标 screen space
-			TransToScreenSpace(p, 3);
+			NDCToScreenSpace(p, 3);
 
 			if constexpr (bool(render_flag & RF_CULL_BACK))
 			{
@@ -207,12 +217,12 @@ namespace core
 			int y1 = (int)Clamp(q[2].y, 1.f, context.back_buffer_view.h - 1.f);
 			int y2 = (int)Clamp(q[0].y, 1.f, context.back_buffer_view.h - 1.f);
 
+			//其实仔细想想，用一次循环来扫描行，并不是很高效，用标准做法分解成上下2个3角形效率更高一点，因为不用再循环里判断点到底与那条直线相交了，不过好歹是我自己想出来的，所以保留了
 			//从上到下扫描
-
 #pragma omp parallel for num_threads(6)
 			for (int y = y2; y >= y1; --y)
 			{
-				//隔行扫描
+				//隔行扫描，就是奇数帧扫描奇数行，偶数帧扫描偶数行，按理说能节省一半着色开销
 				//if (((size_t)y & 1) == context.interlaced_scanning_flag) continue;
 
 				//计算出直线 y = y 与 三角形相交2点的x坐标
@@ -220,6 +230,7 @@ namespace core
 				//float b = q[0].y - k * q[0].x;
 				//float x1 = ((float)y - b) / k;
 
+				//直接把点带入直线方程
 				float x1 = ((float)y + 0.5f - q[0].y) * (q[2].x - q[0].x) / (q[2].y - q[0].y) + q[0].x;
 				float x2 = 0;
 
@@ -256,6 +267,7 @@ namespace core
 			}
 		}
 
+		//像素着色过程，采用了简单的抗锯齿算法，利用了MSAA的思想，不过没有增加采样点深度buffer信息，所以面片之间的显示会出一些问题，混合模式改成线性叠加（还没实现）或许能够解决一部分问题
 		void PixelProcessing_AA(int x, int y, Vec2* triangle, vs_out_t* p0, vs_out_t* p1, vs_out_t* p2)
 		{
 			static_assert(std::is_same_v<Color, fs_out_t>, "Error: 不支持GBuffer");
@@ -263,18 +275,21 @@ namespace core
 			float cover_count = 0;
 			constexpr size_t Mn = 9;
 
+			//采样点
 			constexpr Vec2 simpler[9] = {
 				 Vec2{-.15f,.35f},Vec2{.35f,.15f},Vec2{.15f,-.35f},Vec2{-.35f,-.15f},
 				 Vec2{-.45f,.05f},Vec2{.05f,.45f},Vec2{.45f,-.05f},Vec2{-.05f,-.45f},
 				 Vec2{.0f,.0f}
 			};
 
+			//采样点权重
 			constexpr float simpler_weight[9] = {
 				5,5,5,5,
 				4,4,4,4,
 				9
 			};
 
+			//权重之和，（最后要除）
 			constexpr float simpler_weight_sum[9] = {
 				5,10,15,20,
 				24,28,32,36,
@@ -359,6 +374,7 @@ namespace core
 			context.depth_buffer_view.Set(x, y, depth);
 		}
 
+		// 不带AA的pixel processing
 		void PixelProcessing_NoAA(int x, int y, Vec2* triangle, vs_out_t* p0, vs_out_t* p1, vs_out_t* p2)
 		{
 			Vec3 weight = GetInterpolationWeight(x + 0.5f, y + 0.5f, triangle);
@@ -417,6 +433,7 @@ namespace core
 			}
 		}
 
+		//简单剔除，如果三角形有一个点在CVV之外，就全部剔除
 		bool SimpleCull(vs_out_t triangle[3])
 		{
 			for (size_t i = 0; i < 3; ++i)
@@ -437,7 +454,7 @@ namespace core
 			return false;
 		}
 
-		//如果三个点都在CVV之外,直接剔除
+		//如果三角形三个点都在CVV之外,直接剔除
 		bool CVVCull(vs_out_t triangle[3])
 		{
 			float w0 = triangle[0].position.w;
@@ -467,6 +484,7 @@ namespace core
 			return false;
 		}
 
+		// 在某个平面内，这里的内指的是，该点和平面某点连线与平面法线的点乘为负
 		bool IsInside(vs_out_t* p, int plane)
 		{
 			float x = p->position.x;
@@ -476,8 +494,8 @@ namespace core
 
 			switch (plane)
 			{
-			case 'z>0':  return z > 0;
-			case 'w>e':  return w > epsilon;
+			case 'z>0':  return z > 0;	// 这个叫多字节字符字面量，其实直接用字符串字面量是一样的效果，这里是为了偷懒，为了不额外定义6个面的枚举
+			case 'w>e':  return w > epsilon; // epsilon 是一个很小的量，直接在w=0平面上剔除，可能会导致原本z=0的顶点被剔除掉
 			case 'z<w':  return z < w;
 			case 'x<w':  return x < w;
 			case 'x>-w': return x > -w;
@@ -487,6 +505,7 @@ namespace core
 			return false;
 		}
 
+		// 获得线段与cvv6个平面中某个平面的交点
 		vs_out_t GetClipIntersection(vs_out_t* p0, vs_out_t* p1, int plane)
 		{
 			//Q[x,y,z,w] = P1[x1,y1,z1,w1] + (P2[x2,y2,z2,w2] - P1[x1,y1,z1,w1]) * t
@@ -500,6 +519,7 @@ namespace core
 			float w1 = p0->position.w;
 			float w2 = p1->position.w;
 
+			//计算 p-p0 与 p1-p的 长度之比
 			switch (plane)
 			{
 			case 'z>0':  t = z1 / (z1 - z2); break;
@@ -511,9 +531,11 @@ namespace core
 			case 'y>-w': t = -(w1 + y1) / ((y2 - y1) + (w2 - w1)); break;
 			}
 
+			//根据长度之比，用相似三角形法则，通过插值计算出p点坐标
 			return (*p0) * (1 - t) + (*p1) * t;
 		}
 
+		// 让多边形与某个平面裁剪，如果裁剪前是凸，的裁剪后也必定是凸的，返回值为产生的新的多边形的大小
 		size_t ClipAgainstPlane(vs_out_t* polygon_in, size_t len, vs_out_t* polygon_out, int plane)
 		{
 			size_t len_out = 0;
@@ -521,7 +543,7 @@ namespace core
 			{
 				vs_out_t* p0 = polygon_in + i;
 				vs_out_t* p1 = polygon_in + ((i + 1) % len);
-				//sutherland_hodgman算法
+				//sutherland_hodgman算法 https://www.geeksforgeeks.org/polygon-clipping-sutherland-hodgman-algorithm-please-change-bmp-images-jpeg-png/
 				//检查p0, p1 是否在内侧,
 				int b_p0_inside = IsInside(p0, plane);
 				int b_p1_inside = IsInside(p1, plane);
@@ -557,7 +579,7 @@ namespace core
 			return len_out;
 		}
 
-		//三角形与CVV相交, 裁剪并计算插值
+		//三角形与CVV相交，裁剪，并计算生成多边形的顶点数量
 		size_t CVVClip(vs_out_t* polygon_in, vs_out_t* polygon_out)
 		{
 			size_t len = 3;
@@ -571,7 +593,8 @@ namespace core
 			return len;
 		}
 
-		void TransToScreenSpace(vs_out_t* polygon, size_t len) const
+		// 将三角形顶点从, NDC变换到屏幕坐标系
+		void NDCToScreenSpace(vs_out_t* polygon, size_t len) const
 		{
 			for (int i = 0; i < len; ++i)
 			{
@@ -585,7 +608,8 @@ namespace core
 			}
 		}
 
-		void TransToScreenSpace(Vec2* triangle, size_t len) const
+		// 将三角形顶点从, NDC变换到屏幕坐标系, 也可以用一个3x3矩阵来做，
+		void NDCToScreenSpace(Vec2* triangle, size_t len) const
 		{
 			for (int i = 0; i < len; ++i)
 			{
@@ -599,6 +623,7 @@ namespace core
 			}
 		}
 
+		// 判断正反面， 我这里设置顺时针为正面
 		bool IsBackface(Vec2* triangle) const
 		{
 			Vec2 a = triangle[1] - triangle[0];
@@ -607,6 +632,7 @@ namespace core
 			return a.Cross(b) < 0;//&& b.Cross(c) > 0 && c.Cross(a) > 0;
 		}
 
+		// 利用叉乘判断点是否在三角形中
 		bool IsPointInTriangle2D(float x, float y, vs_out_t* triangle) const
 		{
 			Vec2 p = { x,y };
@@ -617,7 +643,7 @@ namespace core
 			return (pa.Cross(pb) > 0 && pb.Cross(pc) > 0 && pc.Cross(pa) > 0) || (pa.Cross(pb) < 0 && pb.Cross(pc) < 0 && pc.Cross(pa) < 0);
 		}
 
-		//获取插值
+		// 获取插值 三角形重心坐标插值
 		Vec3 GetInterpolationWeight(float x, float y, Vec2* triangle) const
 		{
 			//t*p0+u*p0+v*p1=p, t=1-u-v;
@@ -634,7 +660,7 @@ namespace core
 		}
 
 	protected:
-		Context<fs_out_t>& context;
+		Context<fs_out_t>& context; //这个fs_out_t可以是color也可以是Gbuffer
 		const Shader& shader;
 	};
 }
